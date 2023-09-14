b629991abdd75ca3e18ebc643a44969a6034b3b2
ismilearefin <arefinismile@gmail.com>
2023-09-14 20:34:37 +0600

    Responsive Navbar menu button

--- src/components/AuthLink/AuthLink.js
+++ src/components/AuthLink/AuthLink.js
@@ -1,19 +1,47 @@
+"use client"
+
 import Link from 'next/link';
-import React from 'react';
+import React, { useState } from 'react';
 import styles from "./authLink.module.css"
 
 export default function AuthLink() {
+
+  const [open,setOpen] = useState(true)
+
+
   const status = 'notAuthenticated'
   return (
     <>
     {status === 'notAuthenticated'? (
-      <Link href="/login">Login</Link>
+      <Link href="/login" className={styles.link}>Login</Link>
     ):(
       <>
       <Link href="/write">Write</Link>
       <span className={styles.link}>logout</span>
       </>
     )}
+    <div className={styles.burger} onClick={()=>setOpen(!open)}>
+    <div className={styles.line}></div>
+    <div className={styles.line}></div>
+    <div className={styles.line}></div>
+    </div>
+    {
+      open && (
+        <div className={styles.responsiveMenu}>
+          <Link href='/'>Home</Link>
+          <Link href='/'>About</Link>
+          <Link href='/'>Contact</Link>
+          {status === 'notAuthenticated'? (
+            <Link href="/login" className={styles.link}>Login</Link>
+          ):(
+            <>
+            <Link href="/write">Write</Link>
+            <span className={styles.link}>logout</span>
+            </>
+          )}
+        </div>
+      )
+    }
     </>
   )
 }
--- src/components/themeToggle/ThemeToggle.js
+++ src/components/themeToggle/ThemeToggle.js
@@ -1,17 +1,32 @@
-"use client"
-import React, { useContext } from 'react'
-import styles from './themeToggle.module.css'
-import Image from 'next/image'
-import { themeContext } from '@/context/ThemeContext'
+"use client";
+import React, { useContext } from "react";
+import styles from "./themeToggle.module.css";
+import Image from "next/image";
+import { themeContext } from "@/context/ThemeContext";
 
 export default function ThemeToggle() {
-  const {theme,toggle} = useContext(themeContext);
-  
+  const { theme, toggle } = useContext(themeContext);
+
   return (
-    <div className={styles.container} onClick={toggle} style={theme === "dark"? { backgroundColor:"white"}:{ backgroundColor:"#0f172a"}}>
-      <Image src='/moon.png' alt='moon' width={14} height={14}></Image>
-      <div className={styles.ball} style={theme === "dark"? { left:1,backgroundColor:"#0f172a"}:{right:1, backgroundColor:"white"}}></div>
-      <Image src='/sun.png' alt='sun' width={14} height={14}></Image>
+    <div
+      className={styles.container}
+      onClick={toggle}
+      style={
+        theme === "dark"
+          ? { backgroundColor: "white" }
+          : { backgroundColor: "#0f172a" }
+      }
+    >
+      <Image src="/moon.png" alt="moon" width={14} height={14}></Image>
+      <div
+        className={styles.ball}
+        style={
+          theme === "dark"
+            ? { left: 1, backgroundColor: "#0f172a" }
+            : { right: 1, backgroundColor: "white" }
+        }
+      ></div>
+      <Image src="/sun.png" alt="sun" width={14} height={14}></Image>
     </div>
-  )
+  );
 }
