e4383fc27a6f34396ac4e084771af8a324e18ac9
ismilearefin <arefinismile@gmail.com>
2023-09-14 22:27:44 +0600

    Category list section design & Fix scroll related error

--- src/components/categoryList/CategoryList.js
+++ src/components/categoryList/CategoryList.js
@@ -1,7 +1,65 @@
+import Image from 'next/image'
+import Link from 'next/link'
+import styles from './categoryList.module.css'
 import React from 'react'
+
+const data = [
+  {
+    _id:"01",
+    title:"Style",
+    img:"/style.png"
+  },
+  {
+    _id:"02",
+    title:"Fashion",
+    img:"/fashion.png"
+  },
+  {
+    _id:"03",
+    title:"Food",
+    img:"/food.png"
+  },
+  {
+    _id:"04",
+    title:"Travel",
+    img:"/travel.png"
+  },
+  {
+    _id:"05",
+    title:"Culture",
+    img:"/culture.png"
+  },
+  {
+    _id:"06",
+    title:"Coding",
+    img:"/coding.png"
+  }
+]
 
 export default function CategoryList() {
   return (
-    <div>CategoryList</div>
+    <div>
+      <h1 className={styles.title}>Popular Categories</h1>
+      <div className={styles.categories}>
+        {data?.map((item) => (
+          <Link
+            href={`/blog?cat=${item.title}`}
+            className={`${styles.category} ${styles[item.title]}`}
+            key={item._id}
+          >
+            {item.img && (
+              <Image
+                src={item.img}
+                alt=""
+                width={32}
+                height={32}
+                className={styles.image}
+              />
+            )}
+            {item.title}
+          </Link>
+        ))}
+      </div>
+    </div>
   )
 }
